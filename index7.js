e77846d7ebd9850155e7b28d58aa9ec9b679e68a
Soumya2000-giet <soumyaranjanpradhan734@gmail.com>
2024-08-15 22:22:49 +0530

    updated code

--- index7.js
+++ index7.js
@@ -1,4 +1,23 @@
-const form_e = document.getElementsByTagName('form')
-const input_e = document.getElementById('')
+const form_e = document.querySelector('form')
+const btn = document.querySelector('button')
+const fruit_add = document.querySelector('.fruits')
 
 const new_input = document.createElement('input')
+new_input.id="description"
+new_input.type="text"
+
+form_e.insertBefore(new_input, btn)
+
+form_e.addEventListener('submit', function(event){
+
+    const fruit_e = document.querySelector('#fruit-to-add')
+    const desc_e = document.querySelector("#description")
+    
+    if (fruit_e){
+        const new_li = document.createElement('li')
+        new_li.innerHTML=fruit_e.value+`<br><p>${desc_e.value}<p>`+'<button class="delete-btn">x</button>'
+        fruit_add.appendChild(new_li)
+    }
+
+
+})
