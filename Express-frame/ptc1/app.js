34cd86c1a095ae5a0c33a2ec6a1e8c9fa69f9ea3
Soumya2000-giet <soumyaranjanpradhan734@gmail.com>
2025-01-12 00:26:07 +0530

    Routers and filters task1

--- Express-frame/ptc1/app.js
+++ Express-frame/ptc1/app.js
@@ -1,27 +1,20 @@
-const http = require('http')
 
-const body_parser = require('body-parser')
 const Express = require('express')
 
 const app = Express();
 
-app.use(body_parser.urlencoded({extended : false}))
+const adminrouter = require('./routes/admin')
 
+const shoprouter = require('./routes/shop')
 
-app.use('/add-product',(request, response, next)=>{
-    console.log("this is 2nd middlewire")
-    response.send("<form action ='/product' method='post'><label for='name'>Name</label><input type='text' id='name' name='title'><br><label for='size'>size</label><input type='text' id='size' name='size'><br><button type='submit'>add</button></form>")
+app.use('/admin',adminrouter)
+
+app.use('/shop',shoprouter)
+
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>status not found</h1>')
 })
-app.use('/product',(request, response, next)=>{
-    console.log(request.body)
-    response.redirect('/')
-})
 
-
-app.use('/',(request, response, next)=>{
-    response.send("<h1>hello from 1st page</h1>")
-    console.log("this is 1st middlewire");
-})
 
 app.listen(3000,()=>{
     console.log("server is running")
