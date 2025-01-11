5df111662e92ab9016513d7409b51d67c5bc078b
Soumya2000-giet <soumyaranjanpradhan734@gmail.com>
2025-01-11 13:21:14 +0530

    addd new input

--- Express-frame/ptc1/app.js
+++ Express-frame/ptc1/app.js
@@ -10,7 +10,7 @@ app.use(body_parser.urlencoded({extended : false}))
 
 app.use('/add-product',(request, response, next)=>{
     console.log("this is 2nd middlewire")
-    response.send("<form action ='/product' method='post'><label for='name'>Name</label><input type='text' id='name' name='title'><button type='submit'>add</button></form>")
+    response.send("<form action ='/product' method='post'><label for='name'>Name</label><input type='text' id='name' name='title'><br><label for='size'>size</label><input type='text' id='size' name='size'><br><button type='submit'>add</button></form>")
 })
 app.use('/product',(request, response, next)=>{
     console.log(request.body)
