f34dc39080c021af01d77860abd8694304965922
Soumya2000-giet <soumyaranjanpradhan734@gmail.com>
2025-02-25 23:07:53 +0530

    added code for task3

--- Express-frame/04-adding-a-cart-model/controllers/shop.js
+++ Express-frame/04-adding-a-cart-model/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -9,8 +10,6 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
-
-
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
@@ -40,11 +39,13 @@ exports.getCart = (req, res, next) => {
   });
 };
 
-exports.postCart = (req, res ,next) =>{
-  console.log(req.body.product_ID)
-  res.redirect('/cart')
-
-}
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
