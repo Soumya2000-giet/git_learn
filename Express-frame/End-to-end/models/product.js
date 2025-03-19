4e7ae0621389504a952c995545f7dcc95e56155a
Soumya2000-giet <soumyaranjanpradhan734@gmail.com>
2025-03-19 21:54:26 +0530

    added code for sequelize

--- Express-frame/End-to-end/models/product.js
+++ Express-frame/End-to-end/models/product.js
@@ -1,44 +1,33 @@
-const fs = require('fs');
-const path = require('path');
-
-const Cart = require('./cart');
-
-const db = require('../util/database')
 
 
 
-// const p = path.join(
-//   path.dirname(process.mainModule.filename),
-//   'data',
-//   'products.json'
-// );
+const Sequelize = require('sequelize')
 
-const getProductsFromFile = cb => {
- 
-};
+sequelize = require('../util/database')
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const product = sequelize.define('product',{
+  id : {
+    type:Sequelize.INTEGER,
+    autoIncrement : true,
+    allowNull :false,
+    primaryKey :true
+  },
+  title :{
+    type:Sequelize.STRING
+  },
+  price:{
+    type:Sequelize.DOUBLE,
+    allowNull:false
+  },
+  imageUrl:{
+    type:Sequelize.STRING,
+    allowNull:false
+  },
+  description:{
+    type:Sequelize.STRING,
+    allowNull:true
   }
 
-  save() {
-    return db.execute("insert into products(title,price,description,imageURL) value(?,?,?,?)",[this.title, this.price,this.description,this.imageUrl,])
-  }
+})
 
-  static deleteById(id) {
-    
-  }
-
-  static fetchAll() {
-    return db.execute("select * from products")
-  }
-
-  static findById(id) {
-  return db.execute('select * from products where products.id=?',[id])
-  }
-};
+module.exports = product
--- Express-frame/End-to-end/util/database.js
+++ Express-frame/End-to-end/util/database.js
@@ -1,11 +1,18 @@
-const sql = require('mysql2')
+// const sql = require('mysql2')
 
-const pool = sql.createPool({
-    host :'localhost',
-    user :'root',
-    database:'roo_db',
-    password:'123456'
+// const pool = sql.createPool({
+//     host :'localhost',
+//     user :'root',
+//     database:'roo_db',
+//     password:'123456'
 
-})
+// })
 
-module.exports = pool.promise();
+// module.exports = pool.promise();
+
+
+const Sequelize = require('sequelize')
+
+const connection = new Sequelize('roo_db','root','123456',{dialect:'mysql',host:'localhost'})
+
+module.exports = connection
