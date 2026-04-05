96a03705e1f0b98458ace8a0d4d6b254e7fd2adc
Soumya2000-giet <soumyaranjanpradhan734@gmail.com>
2026-04-05 21:15:50 +0530

    code added/

--- ExpenseTracker_live/public/index.js
+++ ExpenseTracker_live/public/index.js
@@ -391,3 +391,15 @@ document.getElementById("itemsPerPage").addEventListener("change", () => {
   loadExpenses(Items_per_page); // reload expenses
 });
 
+function logoutUser() {
+
+   
+    localStorage.removeItem("token");
+
+    
+    localStorage.removeItem("isPremium");
+
+  
+    window.location.href = "login.html";
+}
+
